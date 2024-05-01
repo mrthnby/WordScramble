d20fe2860966df27237fa27ff755262471d084e6
mrthnby <meralihanbay@gmail.com>
2024-05-01 18:34:59 +0300

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Mert Ali Hanbay on 1.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
